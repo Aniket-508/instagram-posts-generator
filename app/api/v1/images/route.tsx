fff6432195840408f97de9c5960645eefc869cfe
Aniket-508 <pawaraniket508@gmail.com>
2025-01-12 17:05:56 +0530

    fix: apiv1/images route

--- app/api/v1/images/route.tsx
+++ app/api/v1/images/route.tsx
@@ -1,5 +1,5 @@
 import { ImageResponse } from "next/og"
-import { NextRequest } from "next/server"
+import { type NextRequest } from "next/server"
 
 import { getFontsFromTemplate, getFontUrl } from "@/lib/fonts"
 import { templateSchema } from "@/lib/templates"
@@ -7,35 +7,41 @@ import { templates } from "@/components/templates"
 
 export const runtime = "edge"
 
-export const POST = async (request: NextRequest) => {
-  const body = await request.json()
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json()
 
-  const template = templateSchema.parse(body)
-  const fonts = getFontsFromTemplate(template.params)
-  const fontsResponses = await Promise.all(
-    fonts.map((f) =>
-      // Next.js automatically caches fetch requests
-      fetch(getFontUrl({ family: f.family, weight: f.weight }))
+    const template = templateSchema.parse(body)
+    const fonts = getFontsFromTemplate(template.params)
+    const fontsResponses = await Promise.all(
+      fonts.map((f) =>
+        // Next.js automatically caches fetch requests
+        fetch(getFontUrl({ family: f.family, weight: f.weight }))
+      )
     )
-  )
-  const fontBuffers = await Promise.all(
-    fontsResponses.map((res) => res.arrayBuffer())
-  )
+    const fontBuffers = await Promise.all(
+      fontsResponses.map((res) => res.arrayBuffer())
+    )
 
-  const { Template } = templates[template.name]
+    const { Template } = templates[template.name]
 
-  const response = new ImageResponse(<Template template={template} />, {
-    width: template.canvas.width,
-    height: template.canvas.height,
-    fonts: fonts.map((f, i) => {
-      return {
-        name: f.family,
-        weight: f.weight,
-        data: fontBuffers[i],
-        style: "normal",
-      }
-    }),
-  })
-
-  return response
+    return new ImageResponse(<Template template={template} />, {
+      width: template.canvas.width,
+      height: template.canvas.height,
+      fonts: fonts.map((f, i) => {
+        return {
+          name: f.family,
+          weight: f.weight,
+          data: fontBuffers[i],
+          style: "normal",
+        }
+      }),
+    })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    console.log(`${e.message}`)
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    })
+  }
 }
