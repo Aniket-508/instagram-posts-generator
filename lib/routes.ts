7b3c15d43e59fe3159c9ff1ebe3939e015ba773b
Aniket-508 <pawaraniket508@gmail.com>
2025-01-19 18:48:38 +0530

    fix: base route

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `http://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+export const BASE_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
 
 export const API_BASE_URL = `${BASE_URL}/api`
 
