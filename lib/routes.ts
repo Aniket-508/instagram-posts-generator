0a6a15100a95e1308e4dda7a5d584f6d307b101b
Aniket-508 <pawaraniket508@gmail.com>
2025-03-22 12:14:37 +0530

    chore: updated BASE_URL

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,8 +1,8 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development" ||
-  !process.env.VERCEL_PROJECT_PRODUCTION_URL
+  !process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
     ? "http://localhost:3000"
-    : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
 
 export const API_BASE_URL = `${BASE_URL}/api`
 
